7adfb37a29da365afabca5b126df5dfbe36f2f39
Philip Werk <philip.werk@tum.de>
2017-11-16 15:35:47 +0100

    Function getMaterials(), matToFem(), plyToFem() hinzugefügt

--- Layup.m
+++ Layup.m
@@ -4,29 +4,30 @@ classdef Layup
         plys_o = {};
         plys_i = {};
         core = nan;
+        materials = {};
     end
     
     methods
         function obj = Layup()
         end
         
-        function [plys_o,plys_i,core] = get_layup(obj)
+        function [plys_o,plys_i,core] = getLayup(obj)
             plys_o = obj.plys_o;
             plys_i = obj.plys_i;
             core = obj.core;
         end
         
-        function [plys_o, plys_i] = get_plys(obj)
-            plys_o = obj.plys_i;
-            plys_i = obj.plys_o;
+        function [plys_o, plys_i] = getPlys(obj)
+            plys_o = obj.plys_o;
+            plys_i = obj.plys_i;
         end
         
-        function core = get_core(obj)
+        function core = getCore(obj)
             core = obj.core;
         end
         
-        function obj = add_ply(obj,newply,outside)
-            [plycount_o, plycount_i] = obj.count_plys();
+        function obj = addPly(obj,newply,outside)
+            [plycount_o, plycount_i] = obj.countPlys();
             if outside == true
                 obj.plys_o{plycount_o+1} =  newply;
             else
@@ -34,20 +35,20 @@ classdef Layup
             end
         end
         
-        function obj = add_core(obj,core)
+        function obj = addCore(obj,core)
             obj.core =  core;
         end
         
-        function [plycount_o, plycount_i] = count_plys(obj)
+        function [plycount_o, plycount_i] = countPlys(obj)
             plycount_o = length(obj.plys_o);
             plycount_i = length(obj.plys_i);
         end
         
         function ABD = ABD(obj)
             ABD = zeros(6);
-            [t_l,t_c] = obj.get_thickness();
+            [t_l,t_c] = obj.getThickness();
             z1 = -t_l/2;
-            [plycount_o, plycount_i] = obj.count_plys();
+            [plycount_o, plycount_i] = obj.countPlys();
             for i=1:plycount_o
                 z2 = z1 + obj.plys_o{i}.t;
                 ABD = ABD + obj.plys_o{i}.ABD(z1,z2);
@@ -62,9 +63,9 @@ classdef Layup
             end
         end
         
-        function [t_l, t_c, t_so, t_si] = get_thickness(obj)
+        function [t_l, t_c, t_so, t_si] = getThickness(obj)
             t_l = 0;    t_so = 0;   t_si = 0;   t_c = 0;
-            [plycount_o, plycount_i] = obj.count_plys();
+            [plycount_o, plycount_i] = obj.countPlys();
             for i = 1:plycount_o
                 t_so = t_so + obj.plys_o{i}.t;
             end
@@ -78,10 +79,10 @@ classdef Layup
             end
         end
         
-        function [Ex,Ey,NUxy,Gxy] = calc_effConst(obj)
+        function [Ex,Ey,NUxy,Gxy] = calcEffConst(obj)
             ABD = obj.ABD();
             A = ABD(1:3,1:3);
-            [~,~,t_so, t_si] = obj.get_thickness();
+            [~,~,t_so, t_si] = obj.getThickness();
             t = t_so + t_si;
             Ex = EbarX(A,t);
             Ey = EbarY(A,t);
@@ -89,6 +90,60 @@ classdef Layup
             Gxy = GbarXY(A,t);            
         end
         
+        function materials = getMaterials(obj)
+            layup = obj.getLayup;
+            materials = cell(1);
+            for i=1:length(layup)
+                for j = 1:length(layup(i))
+                    component = layup(i);
+                    compMat = component{j}.getProperties();
+                    for k = 1:length(materials)
+                        if isequal(materials{k},compMat)== 1
+                            break;
+                        end
+                        if k==length(materials)
+                           materials = {materials{:},compMat };
+                        end
+                    end
+                end
+            end
+            materials = materials(2:end);
+            obj.materials = materials;
+        end
+        
+        function matFem = matToFem(obj)
+        %Return material in .fem file format
+        mat = obj.getMaterials();
+        matFem = {};
+        for i = 1:length(mat)
+            matFem(i,:) = formatMat(mat{i},i);
+        end
+        end
+
+        function plyFem = plyToFem(obj)
+        %Return material in .fem file format
+        [plys_o,plys_i] = obj.getPlys();
+        mat = obj.getMaterials();
+        plyFem = {};
+        for i = 1:length(plys_o)
+            id = i;
+            MID = getMatId(mat,plys_o{i});
+            plyFem(id,:) = formatPly(plys_o{i},id,MID,1);
+        end
+        for i = 1:length(plys_i)
+            id = length(plys_o)+i;
+            MID = getMatId(mat,plys_i{i});
+            plyFem(id,:) = formatPly(plys_i{i},id,MID,2);
+        end
+        
+        row1 = '$HMNAME LAMINATES              1"laminate1"';
+        row2 = '$HWCOLOR LAMINATES             1       5';
+        row3 = 'STACK,1';
+        for i = 1:id
+            row3 = [row3,',',num2str(i)];
+        end
+        plyFem(id+1,:) = {row1,row2,row3,''};
+        end
     end
 end
 
